279339af6ba3d95a84e8b2afcb0e0227708afeb7
17733101396 <17733101396@163.com>
2019-03-23 21:53:05 +0800

    2019/3/23

--- myweb/resources/views/welcome.blade.php
+++ myweb/resources/views/welcome.blade.php
@@ -38,7 +38,9 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello</div>
+                <div class="title">Hello
+				     
+				</div>
             </div>
         </div>
     </body>
